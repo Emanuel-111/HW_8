fc00d258682be19ec6a15332de7cd7b6dacbe7d6
Emanuel-111 <80918928+Emanuel-111@users.noreply.github.com>
2022-11-11 23:52:12 -0500

    HW_8 Quizzo - EML

--- app/src/main/java/com/hfab/quizzoappeml/MainActivity.java
+++ app/src/main/java/com/hfab/quizzoappeml/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Emanuel "Manny" Luna
+ *
+ * This app allows the user to be quizzed with a couple questions built into the
+ * program. As well as being able to add their own questions.
+ *
+ * If the user gets a 70% or higher, they pass the quiz.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
